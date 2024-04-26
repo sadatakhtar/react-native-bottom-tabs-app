385f787435d3363fd6b9ed655c8708b8073279a5
sadat <sadat0akhtar@gmail.com>
2024-04-26 19:53:54 +0100

    Added placeholder for Biometrics consent screen

---
+++ screens/BiometricsConsent.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const BiometricsConsent = (props: Props) => {
+  return (
+    <View>
+      <Text>BiometricsConsent</Text>
+    </View>
+  )
+}
+
+export default BiometricsConsent
+
+const styles = StyleSheet.create({})
